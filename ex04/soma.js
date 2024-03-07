c34e4c3af5a13a29b0a41d9a4296ba3cdbda307a
Gabriel <gabriel.del.pacheco@gmail.com>
2024-03-07 19:57:55 -0300

    somando valores do array usando reduce

---
+++ ex04/soma.js
@@ -0,0 +1,14 @@
+const prompt = require ("prompt-sync")();
+
+let numeros = [];
+
+for (let i = 1; i <= 5; i++) {
+    let numero = Number(prompt("Informe um número. "));
+    numeros.push(numero);
+}
+
+ let soma = numeros.reduce(function(valInicial, valSeguin){
+    return valInicial+valSeguin
+ },0)
+console.log(numeros);
+console.log(soma);
